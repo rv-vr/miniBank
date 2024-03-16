546feaf4f01b0ca1ef18864a6e54fc3102ff5c55
rv-vr <qsxdrtgbnmjuy@protonmail.ch>
2024-03-16 13:09:12 -0400

    Minor rewrite, added file handling

--- miniBank.c
+++ miniBank.c
@@ -1,154 +1,195 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
 #include <time.h>
+#include <unistd.h>
+#include <conio.h>
+#include <string.h>
 
-// Global vars
+#define SUCCESS 13
+#define EXIT 14
+#define FORCE_EXIT 69
+
+
 float balance = 1000;
-float amt;
-int ch;
-char* t_types[3] = {"Deposit", "Check Balance", "Withdrawal"};
+struct TransactionData {
+    char TransactionType[14]; // Deposit, Withdrawal, Check Balance
+    float Amount; // Amount transacted
+    float InitialBalance; // Inital balance before transaction
+    float FinalBalance; // Balance after transaction
+    char TransactionDate[500]; // Date of Transcation
+};
 
-// Transaction history database
-struct history {
-    char *Transact_type; // Deposit, CheckBalance, Withdraw
-    float amount; // Amount withdrew/deposited, 0/NULL if CheckBalance
-    float initBalance;
-    float currentBalance;
-    char *dateCreated; // mm/dd/yyyy hh:mm:ss 24 hr format
-} logs[99];
-
-// Record history after transaction
-void readHistory(int type, float amt, float initbal, float bal){ // Record History after transaction
+int PostTransaction(int TransactionType, float amount, float InitialBalance, float CurrentBalance){ // Post transaction duties: append to file, post transaction screen
     
-    int i = 0;
-    int maxElement = 99;
-    time_t t = time(NULL);
-    struct tm* timeFull = localtime(&t);
+    int x;
 
-    while (i <= maxElement) {
-        if (logs[i].Transact_type != NULL) {
-            i++;
-        } else {
-            break;
-        }
+    // Open File
+    FILE* infile;
+    infile = fopen("TransactionData.bin","ab");
+    if (infile == NULL) {
+        perror("Error opening file");
+        return FORCE_EXIT; // Error, exit immediately
+    } 
+
+    // Initialize time
+    time_t CurrentTime = time(NULL);
+    struct tm* TransactionTime = localtime(&CurrentTime);
+    char* TimeString;
+    TimeString = asctime(TransactionTime);
+
+    struct TransactionData TransactionDataIn;
+    char* TransactTypes[3] = {"Withdrawal","Deposit","Check Balance"};
+    
+
+    memcpy(&TransactionDataIn.TransactionDate, TimeString, sizeof(TransactionDataIn.TransactionDate));
+    memcpy(&TransactionDataIn.TransactionType, TransactTypes[TransactionType], sizeof(TransactTypes[TransactionType]));
+    TransactionDataIn.Amount = amount;
+    TransactionDataIn.InitialBalance = InitialBalance;
+    TransactionDataIn.FinalBalance = CurrentBalance;
+
+    // Writes data to data file
+    fwrite(&TransactionDataIn, sizeof(struct TransactionData), 1, infile);
+    fclose(infile);
+
+    // Clear Terminal and show Post-transaction Screen
+    printf("(1) Exit or (2) Go to Home Screen? ");
+    scanf("%d", &x);
+    if (x == 1) {
+        return EXIT;
+    } else if (x == 2) {
+        return SUCCESS;
+    } else {
+        printf("\nInvalid number. Going back to Home Screen...");
+        sleep(3);
+        return EXIT;
     }
-
-    logs[i].Transact_type = t_types[type-1];
-    logs[i].amount = amt;
-    logs[i].initBalance = initbal;
-    logs[i].currentBalance = bal;
-    logs[i].dateCreated = asctime(timeFull);
 }
 
-// Password Protection
-// File Handle
-
-// Main Screen
-int HomeScreen(){
-    system("cls");
-    printf("Hello!\n\nCurrent Balance %.2f\n\n(1) Deposit\n(2) Withdraw\n(3) View History\n(4) Exit\n\nEnter your choice:",balance);
-    scanf("%d", &ch);
-    return ch;
-}
-
-// After action Screen
-int afterAction(){
-    printf("(1) Exit or (2) Go back to Main Screen? ");
-    scanf("%d",&ch);
-        if (ch == 1) 
-        {
-            printf("THANK YOU FOR USING MIBANK, PLEASE COME AGAIN!");
-            getch();
-            return 1;
-        } else if (ch == 2)
-        {
-            return 2;    
-        }
-}
-
-int withDraw(){
-    float initBal = balance;
-    system("cls");
-    printf("Enter amount to withdraw: ");
-    scanf("%f", &amt);
-    balance = balance - amt;
-    printf("Withdraw successful.\n\nCash on Hand: %.2f\nCurrent Balance: %.2f\n\n",amt,balance);
-
-    readHistory(3, amt, initBal, balance);
-
-    ch = afterAction();
-    return ch;
-}
-
-int depoSit(){
-    float initBal;
+int Deposit(){ // Deposit transaction
+    float amount, initialBalance;
     system("cls");
     printf("Enter amount to deposit: ");
-    scanf("%f", &amt);
-    
-    initBal = balance;
-    balance = balance + amt;
-    printf("Deposit successful.\n\nInitial Balance: %.2f\nCurrent Balance: %.2f\n\n",initBal,balance);
+    scanf("%f",&amount);
 
-    readHistory(1, amt, initBal, balance);
-    
-    ch = afterAction();
-    return ch;
+    initialBalance = balance;
+    balance = balance + amount;
+
+    if (PostTransaction(1, amount, initialBalance, balance) == SUCCESS) {
+        return SUCCESS;
+    } else {
+        return EXIT;
+    }
+
 }
 
-void viewHistory(){
-    int i;
-    for (i = 0; i <= 99; i++) {
-        if (logs[i].Transact_type != NULL) {
-        printf("\nType: %s\nAmount: %.2f\nInitial Balance: %.2f\nCurrent Balance: %.2f\nDate: %s\n",logs[i].Transact_type, logs[i].amount, logs[i].initBalance, logs[i].currentBalance, logs[i].dateCreated);
-        } else {
-            break;
-        }
+int Withdraw(){ // Withdraw transaction
+    float amount, initialBalance;
+    initialBalance = balance;
+    system("cls");
+    printf("Available Balance: %.2f\nEnter amount to withdraw: ", initialBalance);
+    scanf("%f",&amount);
+    
+    if (amount > balance) {
+        printf("Invalid amount. Exiting...");
+        sleep(3);
+        return EXIT;
+    } else {
+    balance = balance + amount;
     }
+
+    if (PostTransaction(0, amount, initialBalance, balance) == SUCCESS) {
+        return SUCCESS;
+    } else {
+        return EXIT;
+    }
+}
+
+int ViewHistory(){ // View history
+    system("cls");
+    // Open file for reading
+    int x;
+    struct TransactionData TransactionData;
+    FILE* infile = fopen("TransactionData.bin", "rb");
+    if (infile == NULL) {
+        perror("Error opening file");
+        return EXIT;
+    }
+
+    // Read structs using fseek and fread until EOF
+    int num_read = 0;
+    while (fread(&TransactionData, sizeof(struct TransactionData), 1, infile) == 1) {
+        printf("\nTransaction: %s\nAmount: %.2f\nInitial Balance: %.2f\nCurrent Balance: %.2f\nTransaction Date: %s\n",TransactionData.TransactionType,TransactionData.Amount,TransactionData.InitialBalance,TransactionData.FinalBalance,TransactionData.TransactionDate);
+        num_read++;
+    }
+
+    printf("Read %d entries from the file.\n", num_read);
+    printf("(1) Exit or (2) Go to Home Screen? ");
+    scanf("%d", &x);
+    if (x == 1) {
+        return EXIT;
+    } else if (x == 2) {
+        return SUCCESS;
+    } else {
+        printf("\nInvalid number. Going back to Home Screen...");
+        sleep(3);
+        return EXIT;
+    }
+    fclose(infile);
+    
+    return SUCCESS;
+}
+
+int HomeScreen(){
+    int x;
+    system("cls");
+    printf("Hello\n\nCurrent Balance %.2f\n", balance);
+    printf("(1) Deposit\n(2) Withdraw\n(3) View History\n(4) Exit");
+    printf("\n\nEnter your choice: ");
+    scanf("%d",&x);
+    return x;
 }
 
 int main(){
-    int i = 1;   
-    while (i < 6 && i > 0) { // while loop to loop back to HomeScreen every after action
-        int i2 = HomeScreen();
-        switch (i2) {
+    int i;
+    do {
+        switch (HomeScreen()) {
             case 1:
-                if (depoSit() == 1){
+                if(Deposit() == SUCCESS){
+                    break;
+                } else {
                     i--;
-                } 
-                break;
+                    break;
+                }
             case 2:
-               if (withDraw() == 1){
+                if(Withdraw() == SUCCESS){
+                        break;
+                    } else {
+                        i--;
+                        break;
+                    }
+            case 3:
+                if(ViewHistory() == SUCCESS){
+                    break;
+                } else {
                     i--;
-               }
-               break;
+                    break;
+                }
             case 4:
                 system("cls");
-                printf("THANK YOU FOR USING MIBANK, PLEASE COME AGAIN!");
-                getch();
-                i--;
-                break;
-            case 3:
-                system("cls");
-                viewHistory();
-                if (afterAction() == 1){
-                    i--;
-                }
-                break;
+                printf("Thanks for using miniBank!!!");
+                sleep(3);
+                return 0;
             default:
-                system("cls");
-                printf("Invalid code.");
-                getch();
+                printf("Invalid code. Try again.");
+                sleep(3);
                 system("cls");
                 i++;
                 break;
         }
-        if (i == 5) { // Exits after more than 5 incorrect attempts.
+        if (i == 4) {
             printf("Too many incorrect attempts. Exiting...");
-            getch();
+            sleep(3);
             i++;
         }
-    }
+    } while (i >= 0 && i <= 5);
     return 0;
 }
